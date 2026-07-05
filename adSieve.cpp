3afeb399348b48ba4c9488b8c661301409480705
TobiasWuki <tobias07wuki@gmail.com>
2026-07-05 14:45:18 +0200

    Update: Wildcard-Schleife und Listen-Parser korrigiert

--- adSieve.cpp
+++ adSieve.cpp
@@ -19,42 +19,67 @@ adSieve::adSieve() {
     
      std::ifstream file(filePath);
 
-    if (file.is_open() == false) {
+     //ist das File offen?
+    if (file.is_open() == false) { 
         std::cout << "Fehler: Blacklist nicht gefunden" << std::endl;
         return;
     }
 
     std::string currentLine;
 
-    while (std::getline(file, currentLine)) {
+    //kürzt die DNS Blocker Einträge auf die gewollte Domain. Also keine Kommentare, keine IP, etc
+    while (std::getline(file, currentLine)) {       
 
 		if (currentLine.empty()) {			continue;  } //wenn leer überspringen
 		
-		size_t otherChars = currentLine.find_first_of("!#");
-		
-		if (otherChars != std::string::npos) {
-		continue;			// wenn kommentar überspringen
+        // 2. Wo ist der allererste Buchstabe oder die erste Zahl?
+        int firstLetterPos = -1;
+        for (int i = 0; i < currentLine.length(); i++) {
+            if (std::isalnum(currentLine[i])) {
+                firstLetterPos = i;
+                break;
+            }
         }
-	
-		
-        size_t space = currentLine.find(' '); //size_t = riesiges 64 bit unsigned int
-        // Semantik-Schutz: Nur splitten, wenn wirklich ein Leerzeichen da ist!
-        if (space != std::string::npos) {
-            std::string domain = currentLine.substr(space + 1);		//ip adresse ist immer mit space getrennt, daher wenn space ip adresse davor daher abschneiden
 
-			for (int i = 0; i < domain.length(); i++) {
-				if (std::isalnum(domain[i]) { //es können kommentare oder auch syntax dass es eine wildcard ist, davorstehen. daher jedes zeichen weg
-					
-					domain = currentLine.substr(i-1);
-				}
-			}	
-            hashTable.insert(domain);
+        // 3. Wo ist das erste Kommentarzeichen?
+        size_t commentPos = currentLine.find_first_of("!#");
+
+        // Wenn ein Kommentarzeichen existiert UND es steht VOR dem ersten Buchstaben:
+        if (commentPos != std::string::npos && (firstLetterPos == -1 || commentPos < firstLetterPos)) {
+            continue; // Das ist ein echter Kommentar -> Zeile komplett überspringen!
         }
-			
-        else {
-            // Keine IP davor? Dann ist die ganze Zeile die Domain!
-            hashTable.insert(currentLine);
+        
+        size_t space = currentLine.find_first_of(" \t"); //size_t = riesiges 64 bit unsigned int
+
+        std::string domain = currentLine;
+
+        if (space != std::string::npos) {
+            domain = currentLine.substr(space + 1);		//ip adresse ist immer mit space getrennt, daher wenn space ip adresse davor daher abschneiden
+
         }
+        //for (int i = 1; i <= domain.length(); i++) {
+        //    if (std::isalnum(domain[i-1])) { //es können kommentare oder auch syntax dass es eine wildcard ist, davorstehen. daher jedes zeichen weg
+        //        domain = domain.substr(i - 1);
+        //        break;
+        //    }
+        //}
+        while (!domain.empty() && !std::isalnum(domain.front())) {
+            domain.erase(0, 1); // Löscht das allererste Zeichen (Index 0, Länge 1)
+        }
+
+        //int domainLength = domain.length();
+        //for (int i = 1; i <= domainLength; i++) {
+        //    domainLength = domain.length();
+        //    if (std::isalnum(domain[domainLength - i])) { //es können kommentare oder auch syntax dass es eine wildcard ist, davorstehen. daher jedes zeichen weg
+        //        domain = domain.substr(0, domainLength - i + 1);
+        //        break;
+        //    }
+        //}
+        while (!domain.empty() && !std::isalnum(domain.back())) {
+            domain.pop_back();
+        }
+        hashTable.insert(domain);
+        
     }
 }
 
@@ -113,7 +138,8 @@ bool adSieve::checkBuffer(char* queryBuffer) {
     int position = 12;
     std::string domain = "";
 
-    while (queryBuffer[position] != 0) {
+    //kürzt queryBuffer um den Header etc und macht einen richtigen Domain string für den blocklist vergleich
+    while (queryBuffer[position] != 0) {        
 
 
 
@@ -131,5 +157,29 @@ bool adSieve::checkBuffer(char* queryBuffer) {
             domain += '.';
         }
     }
-	return hashTable.find(domain) != hashTable.end();
+
+    size_t posDot = 0;
+
+    //vergleicht die Blocklist und die angefragte Domain, schneidet auch subDomains weg
+    if (!(hashTable.find(domain) != hashTable.end())) {     //vergleicht die Blocklist und die angefragte Domain, schneidet auch subDomains weg
+
+        while (true) {
+
+            posDot = domain.find('.');
+
+            if (posDot == std::string::npos) {
+                return false;
+                
+            }
+
+            domain = domain.substr(posDot + 1);
+
+            if (hashTable.find(domain) != hashTable.end()) {
+                return true;
+                
+            }
+        }
+    }
+
+    else { return true; }
 }
